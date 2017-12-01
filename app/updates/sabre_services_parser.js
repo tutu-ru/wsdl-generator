f44f5d2c19dd15b190929ffeefb405dab1169b21
Mihail Catirau <mihail.c@avantajprim.com>
2017-12-01 11:11:11 +0200

    Update service parser and services list
    Add path to official documentation
    Add service description

--- app/updates/sabre_services_parser.js
+++ app/updates/sabre_services_parser.js
@@ -11,18 +11,23 @@
 
 
 var t = '[\n';
-$('table tr').each(function(){
+$('table tr:gt(0)').each(function(){
 	t += '\t[\n';
-	$(this).find('td:eq(0)').each(function(){
-		var s = $(this).find('a').text();
-		var v = $(this).find('em').text().replace(/\(/g,'').replace(/\)/g,'');
-		t += '\t\t\'service\' =>\''+ s +'\',\n';
-		t += '\t\t\'version\' =>\''+ v +'\',\n';
-	});
-	$(this).find('td:eq(2)').each(function(){
-		var w = $(this).find('a:last').attr('href');
-		t += '\t\t\'wsdl\' =>\''+ w +'\',\n';
-	});
+
+	var serviceCell = $(this).find('td:eq(0)');
+	var s = $(serviceCell).find('a').text();
+	var v = $(serviceCell).find('em').text().replace(/\(/g,'').replace(/\)/g,'');
+	var o = $(serviceCell).find('a').attr('href');
+	t += '\t\t\'service\' =>\''+ s +'\',\n';
+	t += '\t\t\'version\' =>\''+ v +'\',\n';
+	t += '\t\t\'documentation\' =>\''+ o +'\',\n';
+	var wsdlCell = $(this).find('td:eq(2)');
+	var w = $(wsdlCell).find('a:last').attr('href');
+	t += '\t\t\'wsdl\' =>\''+ w +'\',\n';
+	var descriptionCell = $(this).find('td:eq(1)');
+	var d = $(descriptionCell).text();
+	t += '\t\t\'description\' =>\''+ d +'\',\n';
+
 	t+='\t],\n';
 });
 t+='];\n';
